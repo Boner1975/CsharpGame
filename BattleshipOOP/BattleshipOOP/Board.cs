ad0d0da26b00fa3faa18c619243d8a5bcfd99c41
izazmu <72415592+izazmu@users.noreply.github.com>
2021-04-14 00:29:54 +0200

    BoardFactory, Board

--- BattleshipOOP/BattleshipOOP/Board.cs
+++ BattleshipOOP/BattleshipOOP/Board.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BattleshipOOP
 {
     public class Board
     {
-        Square[,] ocean;
+        private Square[,] ocean;
+        private int size;
+
+        public Board(int size)
+        {
+            this.size = size;
+            ocean = new Square[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    ocean[i, j] = new Square();
+        }
 
         public bool IsPlacementOk
         {
@@ -14,6 +22,39 @@ namespace BattleshipOOP
             {
                 return true; //to be implemented
             }
+        }
+
+        internal bool PlaceShip(Ship ship, int x1, int y1, int x2, int y2)
+        {
+            if (x1 < 0 || x1 >= size ||
+                y1 < 0 || y1 >= size ||
+                x2 < 0 || x2 >= size ||
+                y2 < 0 || y2 >= size
+                )
+            {
+                return false;
+            }
+
+            int shipSize = (int)ship.Type;
+            int sumX = Math.Abs(x2 - x1 + 1);
+            int sumY = Math.Abs(y2 - y1 + 1);
+            
+            if (!(sumY + sumX == shipSize && (sumX == 0 || sumY == 0)))
+            {
+                return false;
+            }
+
+/*            if()
+            {
+                return false;
+            }*/
+
+            return true;
+        }
+
+        private bool CheckSquare(int x,int y)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- BattleshipOOP/BattleshipOOP/BoardFactory.cs
+++ BattleshipOOP/BattleshipOOP/BoardFactory.cs
@@ -1,14 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace BattleshipOOP
 {
     public class BoardFactory
     {
-        public void RandomPlacement()
+        private Board CreateBoard(int size)
         {
-            //to be implemented
+            Board board = new Board(size);
+            return board;
+        }
+
+        private List<Ship> CreateListOfShips()
+        {
+            return new List<Ship> {
+                {new Ship(Ship.ShipType.Carrier) },
+                {new Ship(Ship.ShipType.Battleship) },
+                {new Ship(Ship.ShipType.Battleship) },
+                {new Ship(Ship.ShipType.Cruiser) },
+                {new Ship(Ship.ShipType.Cruiser) },
+                {new Ship(Ship.ShipType.Cruiser) },
+                {new Ship(Ship.ShipType.Destroyer) },
+                {new Ship(Ship.ShipType.Destroyer) },
+                {new Ship(Ship.ShipType.Destroyer) },
+                {new Ship(Ship.ShipType.Destroyer) },
+                {new Ship(Ship.ShipType.Submarine) },
+                {new Ship(Ship.ShipType.Submarine) },
+                {new Ship(Ship.ShipType.Submarine) },
+                {new Ship(Ship.ShipType.Submarine) },
+                {new Ship(Ship.ShipType.Submarine) }
+            };
+        }
+
+        public (Board, List<Ship>) RandomPlacement(int size)
+        {
+            Board board = CreateBoard(size);
+            List<Ship> ships = CreateListOfShips();
+            Random random = new Random();
+            foreach (Ship ship in ships)
+            {
+                int shipSize = (int)ship.Type;
+                int x1, x2, y1, y2;
+                do
+                {
+                    x1 = random.Next(0, size - 1);
+                    y1 = random.Next(0, size - 1);
+
+                    int direction = random.Next(0, 1);
+                    int ascending = random.Next(0, 1);
+
+                    x2 = x1;
+                    y2 = y1;
+                    if (direction == 1)
+                    {
+                        x2 = ChooseDirection(x1, shipSize, ascending);
+                    }
+                    else
+                    {
+                        y2 = ChooseDirection(y1, shipSize, ascending);
+                    }
+                } while (board.PlaceShip(ship, x1,y1,x2,y2));
+            }
+
+            return (board, ships);
+        }
+
+        private static int ChooseDirection(int y1, int shipSize, int ascending)
+        {
+            int y2;
+            if (ascending == 1)
+            {
+                y2 = y1 + shipSize - 1;
+            }
+            else
+            {
+                y2 = y1 - shipSize + 1;
+            }
+
+            return y2;
         }
 
         public void ManualPlacement()
